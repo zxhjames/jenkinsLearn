04c1ec2fc0261e6499a5353f2058c96a26f8abf1
zxhjames <1849483422@qq.com>
2021-02-07 19:39:57 +0800

    init

---
+++ src/main/java/com/example/demo/controller/http.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: 中间件
+ * @description:
+ * @author: 占翔昊
+ * @create 2021-02-07 18:25
+ **/
+
+@RestController
+public class http {
+    @RequestMapping("/")
+    public String SayHello() {
+        return "hello jenkins";
+    }
+}
