96dfd41683e363d3b880d3daa0b94fb2b130afc2
zxhjames <1849483422@qq.com>
2021-02-08 11:04:02 +0800

    hhh

--- src/main/java/com/example/demo/controller/http.java
+++ src/main/java/com/example/demo/controller/http.java
@@ -14,6 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class http {
     @RequestMapping("/")
     public String SayHello() {
-        return "hello jenkins";
+        return "hello jenkins,我是james";
     }
 }
